bb9d7e0a83c85f26d4338c2e2c60b91683f51335
MaesterGray <historycurators@gmail.com>
2024-12-01 12:02:33 +0100

    continuation

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { extractPrice } from '../utils'
+import { extractCurrency, extractDescription, extractPrice } from '../utils'
 import * as cheerio from 'cheerio'
 
 export async function scrapeAmazonProduct(url:string){
@@ -27,14 +27,46 @@ export async function scrapeAmazonProduct(url:string){
 
         const title = $('#productTitle').text().trim()
         const currentPrice = $('.a-offscreen').text().trim().split('$')[1]
-        const originalPrice = extractPrice(
-            $('#priceblock_ourprice'),
-            $('.a-price.a-text-price span.a-offscreen'),
-            $('#listPrice'),
-            $('#priceblock_dealPrice'),
-            $(.a-size-basename.)
-        )
-        console.log( title,currentPrice)
+        
+        function extractOriginalPrice (input:string){
+            const listPriceIndex = input.indexOf("List Price: $");
+
+// If "List Price:" is found in the string
+if (listPriceIndex !== -1) {
+    // Extract the substring starting after "List Price:"
+    const listPricePart = input.substring(listPriceIndex + "List Price: $".length).trim();
+
+    // Find the first space or end of the string after the price
+    const price = listPricePart.split(" ")[0];
+    return price
+} else {
+    return null
+}
+        }
+        const originalPrice = extractOriginalPrice($('.aok-offscreen').text().trim())
+        const outOfStock = $('#availability span').text().trim().toLowerCase() ==='currently unavailable'
+        const images = $('#imgBlkFront').attr('data-a-dynamic-image')||
+        $('#landingImage').attr('data-a-dynamic-image')||'{}'
+        const imageUrls = Object.keys(JSON.parse(images))
+        const currency = extractCurrency($('.a-price-symbol'))
+        const discountRate = $('.savingsPercentage').text().replace(/[-%]/g,"")
+        const description = extractDescription($)
+        const data = {
+           url,
+           currency: currency||'$' ,
+           image:imageUrls[0],
+           title,
+           currentPrice:Number(currentPrice),
+           originalPrice:Number(originalPrice),
+           priceHistory:[],
+           discountRate:Number(discountRate),
+           category:'category',
+           reviewCount:100,
+           stars:4.5,
+           isOutOfStock:outOfStock,
+           description
+        }
+        console.log(data)
     } catch (error:any) {
         throw new Error(`Failed to scrape product:${error.message}`)
     }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+import { PriceHistoryItem,Product } from "@/types";
+
 export function extractPrice(...elements:any){
     for (const element of elements){
         const priceText = element.text().trim()
@@ -5,4 +7,32 @@ export function extractPrice(...elements:any){
         if(priceText) return priceText.replace(/\D/g,'')
     }
     return ''
+}
+
+export function extractDescription($: any) {
+    // these are possible elements holding description of the product
+    const selectors = [
+      ".a-unordered-list .a-list-item",
+      ".a-expander-content p",
+      // Add more selectors here if needed
+    ];
+  
+    for (const selector of selectors) {
+      const elements = $(selector);
+      if (elements.length > 0) {
+        const textContent = elements
+          .map((_: any, element: any) => $(element).text().trim())
+          .get()
+          .join("\n");
+        return textContent;
+      }
+    }
+  
+    // If no matching elements were found, return an empty string
+    return "";
+  }
+  
+export function extractCurrency(element:any){
+    const currencyText = element.text().trim().slice(0,1);
+    return currencyText ? currencyText : '';
 }
